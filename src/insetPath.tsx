5376711fe2304fb84a4ac55d0d7c622a3fc3c94e
Jared Forsyth <jared@jaredforsyth.com>
2021-11-25 14:39:33 -0600

    playng around

--- src/insetPath.tsx
+++ src/insetPath.tsx
@@ -4,12 +4,130 @@ import { angleTo, dist, push } from './getMirrorTransforms';
 import {
     circleCircle,
     epsilon,
+    intersections,
     lineCircle,
     lineLine,
     lineToSlope,
 } from './intersect';
 import { angleBetween } from './findNextSegments';
 import { coordsEqual } from './pathsAreIdentical';
+import { pathToPrimitives } from './findSelection';
+import { sortHitsForPrimitive } from './clipPath';
+
+/*
+
+pruneInsetPath.
+
+So, if you have self-intersecting paths
+Here's what you do.
+
+start from an intersection. There will be two "out" directions.
+start at any given "corner" (start of a line segment)
+
+hmmm
+
+how to traverse all of them .. and know that I have?
+
+*/
+
+export type Hit = { first: number; second: number; coord: Coord };
+
+export const pruneInsetPath = (segments: Array<Segment>) => {
+    const primitives = pathToPrimitives(segments);
+    const hits: Array<Array<Hit>> = [];
+    let anyHits = false;
+    for (let i = 0; i < segments.length; i++) {
+        const previ =
+            i === 0 ? segments[segments.length - 1].to : segments[i - 1].to;
+        hits.push([]);
+        for (let j = i + 1; j < segments.length; j++) {
+            const prevj =
+                j === 0 ? segments[segments.length - 1].to : segments[j - 1].to;
+            const these = intersections(primitives[i], primitives[j]);
+            these.forEach((coord) => {
+                const iend =
+                    coordsEqual(coord, previ) ||
+                    coordsEqual(coord, segments[i].to);
+                const jend =
+                    coordsEqual(coord, prevj) ||
+                    coordsEqual(coord, segments[j].to);
+                // This is just two segments meeting. no big deal.
+                // Note that if we managed to get in a place where four lines met in the same place,
+                // this logic would break. here's hoping.
+                if (iend && jend) {
+                    return;
+                }
+                anyHits = true;
+                const hit = { first: i, second: j, coord };
+                hits[i].push(hit);
+                hits[j].push(hit);
+            });
+        }
+    }
+
+    if (!anyHits) {
+        return [segments];
+    }
+
+    const sorted = hits.map((hits, i) =>
+        sortHitsForPrimitive(hits, primitives[i], segments[i]),
+    );
+
+    const seen: { [key: string]: true } = {};
+
+    travelPath(sorted, segments, { idx: 0, hit: -1 }, seen);
+};
+
+type Pos = { idx: number; hit: number };
+
+export const segmentStart = (segments: Array<Segment>, idx: number) =>
+    idx === 0 ? segments[segments.length - 1].to : segments[idx - 1].to;
+
+export const travelPath = (
+    sorted: Array<Array<Hit>>,
+    segments: Array<Segment>,
+    pos: Pos,
+    seen: { [key: string]: true },
+) => {
+    // ok here we go.
+    // keep track of accumulated angle
+    // and all the "sub-segments" you've traveersed, so we can mark them as "done".
+    // because we'll need to check all of the subsegments one by one.
+    let first = segmentStart(segments, pos.idx);
+    let prev = first;
+    let pprev = first;
+    let result: Array<Segment> = [];
+    while (
+        !result.length ||
+        !coordsEqual(result[result.length - 1].to, first)
+    ) {
+        // so, the seg is (the current one)
+        // and ...
+        // where are we?
+        // We're at a cross-road, looking to the future.
+
+        let seg = segments[pos.idx];
+        let hit = sorted[pos.idx][pos.hit + 1];
+        let next: Coord;
+        let nextPos: Pos;
+        if (!hit) {
+            next = seg.to;
+            nextPos = { idx: (pos.idx + 1) % segments.length, hit: -1 };
+        } else {
+            next = hit.coord;
+            hit.coord;
+            nextPos = { idx: pos.idx, hit: pos.hit + 1 };
+        }
+        // If we haven't moved, don't muck
+        if (!coordsEqual(next, prev)) {
+            result.push({ ...seg, to: next });
+            pprev = prev;
+            prev = next;
+        }
+        pos = nextPos;
+    }
+    return result;
+};
 
 export const insetPath = (path: Path, inset: number) => {
     // All paths are clockwise, it just makes this easier
@@ -32,6 +150,11 @@ export const insetPath = (path: Path, inset: number) => {
     // We have a twist!
     // but that's no the only possible self-intersection.
     // If we have a concave shape, it's easy to self-intersect.
+    // So what we need to do is ... find self-intersections, and then
+    // do self-clipping?
+    // Basically do the same kind of clip walk, but it's just on one path.
+    // And we'd have to do the same deal where we look at all points, to see
+    // which ones are in a still-clockwise section of things.
     if (Math.abs(angle) < epsilon * 2) {
         return null;
     }
