2931c336a0795981338d39dacebbd7afe6565733
Jared Forsyth <jared@jaredforsyth.com>
2022-01-29 22:11:17 -0600

    lock to 16:9 so instagram will take it

--- src/AnimationUI.tsx
+++ src/AnimationUI.tsx
@@ -9,6 +9,11 @@ import { canvasRender } from './CanvasRender';
 import { epsilon } from './intersect';
 import { findBoundingRect } from './Export';
 
+export const makeEven = (v: number) => {
+    v = Math.ceil(v);
+    return v % 2 === 0 ? v : v + 1;
+};
+
 export const AnimationEditor = ({
     state,
     dispatch,
@@ -18,6 +23,13 @@ export const AnimationEditor = ({
 }) => {
     const [animationPosition, setAnimationPosition] = React.useState(0);
     const canvas = React.useRef(null as null | HTMLCanvasElement);
+    const [fps, setFps] = React.useState(60);
+
+    const [downloadUrl, setDownloadUrl] = React.useState(null as null | string);
+    const [transcodingProgress, setTranscodingProgress] = React.useState({
+        start: 0.0,
+        percent: 0.0,
+    });
 
     const bounds = React.useMemo(
         () => findBoundingRect(state),
@@ -27,12 +39,15 @@ export const AnimationEditor = ({
     const originalSize = 1000;
     const crop = 10;
 
-    const h = bounds
-        ? (bounds.y2 - bounds.y1) * state.view.zoom + crop * 2
+    let h = bounds
+        ? makeEven((bounds.y2 - bounds.y1) * state.view.zoom + crop * 2)
         : originalSize;
-    const w = bounds
-        ? (bounds.x2 - bounds.x1) * state.view.zoom + crop * 2
+    let w = bounds
+        ? makeEven((bounds.x2 - bounds.x1) * state.view.zoom + crop * 2)
         : originalSize;
+    if (w / h > 16 / 9) {
+        h = (w / 16) * 9;
+    }
 
     React.useEffect(() => {
         if (!canvas.current) {
@@ -50,16 +65,30 @@ export const AnimationEditor = ({
 
     const onRecord = (increment: number) => {
         const ctx = canvas.current!.getContext('2d')!;
+        const images: Array<Uint8Array> = [];
+
+        setTranscodingProgress({ start: Date.now(), percent: 0 });
 
         let i = 0;
         const fn = () => {
             i += increment;
             if (i >= 1 - epsilon) {
+                const blob = tarImages(images, fps, state);
+
+                setDownloadUrl(URL.createObjectURL(blob));
+                setTranscodingProgress({ start: 0, percent: 0 });
                 return;
             }
             ctx.save();
             canvasRender(ctx, state, w * 2, h * 2, 2, i).then(() => {
                 ctx.restore();
+
+                setTranscodingProgress((t) => ({ ...t, percent: i }));
+
+                const dataUrl = canvas.current!.toDataURL('image/jpeg');
+                const data = convertDataURIToBinary(dataUrl);
+                images.push(data);
+
                 requestAnimationFrame(fn);
             });
         };
@@ -74,6 +103,31 @@ export const AnimationEditor = ({
                 height={h * 2}
                 style={{ width: w, height: h }}
             />
+            <div>
+                FPS:{' '}
+                <BlurInt value={fps} onChange={(f) => (f ? setFps(f) : null)} />
+            </div>
+            {transcodingProgress.start === 0 ? null : (
+                <div>
+                    {(transcodingProgress.percent * 100).toFixed(1)} percent
+                    encoded. ETA{' '}
+                    {(
+                        ((Date.now() - transcodingProgress.start) /
+                            1000 /
+                            transcodingProgress.percent) *
+                        (1 - transcodingProgress.percent)
+                    ).toFixed(1)}
+                    seconds
+                </div>
+            )}
+            {downloadUrl ? (
+                <a
+                    href={downloadUrl}
+                    download={new Date().toISOString() + '.tar.jdanim'}
+                >
+                    Download video
+                </a>
+            ) : null}
             <AnimationUI
                 state={state}
                 dispatch={dispatch}
@@ -84,6 +138,57 @@ export const AnimationEditor = ({
         </div>
     );
 };
+
+function tarImages(images: Uint8Array[], fps: number, state: State) {
+    const tar = require('tinytar').tar;
+
+    const args = [
+        '-r',
+        '' + fps,
+        '-i',
+        'image%03d.jpg',
+        '-c:v',
+        'libx264',
+        '-crf',
+        '18',
+        '-pix_fmt',
+        'yuv420p',
+    ];
+    args.push('video.mp4');
+    const res = tar(
+        images
+            .map(
+                (image, i) =>
+                    ({
+                        name: `image${i.toString().padStart(3, '0')}.jpg`,
+                        data: image,
+                    } as any),
+            )
+            .concat([
+                {
+                    name: 'run_ffmpeg.bash',
+                    mode: parseInt('777', 8),
+                    data: `#!/bin/bash
+                                    set -ex
+                                    ffmpeg ${args
+                                        .map((a) =>
+                                            a[0] === '-' ? a : `"${a}"`,
+                                        )
+                                        .join(' ')}
+                                    `,
+                },
+                {
+                    name: `state.json`,
+                    data: JSON.stringify(state),
+                },
+            ]),
+    );
+
+    const blob = new Blob([res], {
+        type: 'tar',
+    });
+    return blob;
+}
 
 export function AnimationUI({
     state,
@@ -101,13 +206,7 @@ export function AnimationUI({
     return (
         <div
             style={{
-                position: 'fixed',
-                bottom: 0,
-                left: 0,
-                right: 0,
-                height: 400,
                 background: 'rgba(0,0,0,0.4)',
-                overflow: 'auto',
                 display: 'flex',
             }}
         >
@@ -425,3 +524,15 @@ export const TickTock = ({
         </div>
     );
 };
+
+function convertDataURIToBinary(dataURI: string) {
+    var base64 = dataURI.replace(/^data[^,]+,/, '');
+    var raw = window.atob(base64);
+    var rawLength = raw.length;
+
+    var array = new Uint8Array(new ArrayBuffer(rawLength));
+    for (let i = 0; i < rawLength; i++) {
+        array[i] = raw.charCodeAt(i);
+    }
+    return array;
+}
