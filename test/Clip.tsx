62cc2e4746030789832066a8afccd66badb37dd1
Jared Forsyth <jared@jaredforsyth.com>
2022-02-18 16:06:43 -0600

    save test cases

--- test/Clip.tsx
+++ test/Clip.tsx
@@ -5,20 +5,38 @@ import { clipPath } from '../src/rendering/clipPath';
 import { Segment } from '../src/types';
 import { Drawing, useLocalStorage } from './Canvas';
 
+type TestCase = { shape: Array<Segment>; clip: Array<Segment> };
+
 export const Clip = () => {
-    const [segments, setSegments] = useLocalStorage('clip-test2', {
+    const [testCases, setTestCases] = useLocalStorage(
+        'clip-test-cases',
+        [] as Array<TestCase>,
+    );
+
+    const [testCase, setTestCase] = useLocalStorage('clip-test2', {
         shape: [],
         clip: [],
-    } as { shape: Array<Segment>; clip: Array<Segment> });
+    } as TestCase);
     const [which, setWhich] = React.useState('shape' as 'shape' | 'clip');
+
+    const size = 500;
     return (
         <div>
+            <button
+                onClick={() => {
+                    setTestCases((t) => t.concat([testCase]));
+                    setTestCase({ shape: [], clip: [] });
+                    setWhich('shape');
+                }}
+            >
+                Add test case
+            </button>
             <Drawing
                 key={which}
                 zoom={1}
-                segments={segments[which]}
+                segments={testCase[which]}
                 setSegments={(s) =>
-                    setSegments((segments) => ({
+                    setTestCase((segments) => ({
                         ...segments,
                         [which]: s(segments[which]),
                     }))
@@ -27,16 +45,16 @@ export const Clip = () => {
                 snap={20}
                 render={(segs) => {
                     const other =
-                        segments[which === 'shape' ? 'clip' : 'shape'];
+                        testCase[which === 'shape' ? 'clip' : 'shape'];
                     if (!other.length) {
                         return;
                     }
                     const clipped =
-                        segments.shape.length > 2 && segments.clip.length > 2
+                        testCase.shape.length > 2 && testCase.clip.length > 2
                             ? clipPath(
-                                  { ...pathSegs(segments.shape), debug: false },
-                                  segments.clip,
-                                  pathToPrimitives(segments.clip),
+                                  { ...pathSegs(testCase.shape), debug: false },
+                                  testCase.clip,
+                                  pathToPrimitives(testCase.clip),
                               )
                             : null;
                     return (
@@ -75,9 +93,50 @@ export const Clip = () => {
             >
                 Clip
             </button>
-            <button onClick={() => setSegments({ shape: [], clip: [] })}>
+            <button onClick={() => setTestCase({ shape: [], clip: [] })}>
                 Clear
             </button>
+            <div
+                style={{
+                    padding: 24,
+                    display: 'flex',
+                    flexWrap: 'wrap',
+                }}
+            >
+                {testCases.map((tc, i) => (
+                    <svg
+                        width={200}
+                        height={200}
+                        viewBox={`-${size / 2} -${size / 2} ${size} ${size}`}
+                    >
+                        <path
+                            stroke="red"
+                            strokeWidth={3}
+                            d={calcPathD(pathSegs(tc.shape), 1)}
+                        />
+                        <path
+                            stroke="blue"
+                            strokeWidth={3}
+                            fill="none"
+                            d={calcPathD(pathSegs(tc.clip), 1)}
+                        />
+                        {clipPath(
+                            pathSegs(tc.shape),
+                            tc.clip,
+                            pathToPrimitives(tc.clip),
+                        ).map((clip, i) => (
+                            <path
+                                stroke="white"
+                                strokeWidth={1}
+                                fill="#aaa"
+                                opacity={0.5}
+                                d={calcPathD(clip, 1)}
+                                key={i}
+                            />
+                        ))}
+                    </svg>
+                ))}
+            </div>
         </div>
     );
 };
