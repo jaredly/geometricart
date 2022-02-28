6c16a01256e100f88602cf20565f627acace18b2
Jared Forsyth <jared@jaredforsyth.com>
2022-02-27 22:46:52 -0600

    join them

--- src/vest/convert.ts
+++ src/vest/convert.ts
@@ -1,31 +1,21 @@
+#!/usr/bin/env yarn -s esbuild --bundle --target=node12 --platform=node
+/**
+ * ./src/vest/convert.ts | node - okfolks
+ */
 import path from 'path';
 import fs from 'fs';
-import { Fixture } from './types';
-import { serializeFixture } from './utils';
+import { deserializeFixture, serializeFixtures } from './utils';
 
 const [_, __, dir] = process.argv;
 
-fs.readdirSync(dir)
-    .filter((n) => n.endsWith('.input.txt'))
-    .forEach((name) => {
-        const base = name.slice(0, -'.input.txt'.length);
-        const full = path.join(dir, name);
-        const outputFull = path.join(dir, base + '.output.txt');
-        const input = JSON.parse(fs.readFileSync(full, 'utf8'));
-        const [pass, outputRaw] = fs
-            .readFileSync(outputFull, 'utf8')
-            .split('\n');
-        const output = JSON.parse(outputRaw);
-        const fixture: Fixture<unknown, unknown> = {
-            name: base.replace(/-/g, ' '),
-            input,
-            output,
-            isPassing: pass === 'pass',
-        };
-        fs.unlinkSync(full);
-        fs.unlinkSync(outputFull);
-        fs.writeFileSync(
-            path.join(dir, base + '.txt'),
-            serializeFixture(fixture, undefined),
-        );
-    });
+const fixtures = fs.readdirSync(dir).map((name) => {
+    const full = path.join(dir, name);
+    const input = fs.readFileSync(full, 'utf8');
+    console.log(full);
+    fs.unlinkSync(full);
+    return deserializeFixture(input, undefined);
+});
+
+fs.unlinkSync(dir);
+const full = serializeFixtures(fixtures, undefined);
+fs.writeFileSync(dir + '.txt', full);
