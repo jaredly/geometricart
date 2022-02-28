fda47b8d33dd7ad6a6a12c70950d72b0439fed30
Jared Forsyth <jared@jaredforsyth.com>
2022-02-27 22:03:36 -0600

    some atBottom tests!

---
+++ src/rendering/SegmentEditor.tsx
@@ -0,0 +1,185 @@
+import * as React from 'react';
+import { RenderSegmentBasic } from '../editor/RenderSegment';
+import { Coord } from '../types';
+import { SegmentWithPrev } from './clipPathNew';
+import { angleTo, dist, push } from './getMirrorTransforms';
+
+export const useInitialState = <T, R = T>(
+    v: T,
+    transform?: (t: T) => R,
+): [R, React.Dispatch<React.SetStateAction<R>>] => {
+    const [current, set] = React.useState(
+        transform ? transform(v) : (v as any as R),
+    );
+    const prev = React.useRef(v);
+    React.useEffect(() => {
+        if (prev.current !== v) {
+            prev.current = v;
+            set(transform ? transform(v) : (v as any as R));
+        }
+    }, [v]);
+    return [current, set];
+};
+
+export const SegmentEditor = ({
+    initial,
+    onChange,
+    children,
+}: {
+    initial: null | SegmentWithPrev;
+    onChange: (p: SegmentWithPrev) => void;
+    children: (
+        current: null | SegmentWithPrev,
+        rendered: React.ReactNode,
+    ) => React.ReactNode;
+}) => {
+    const grid = 15;
+
+    const [current, setCurrent] = useInitialState(initial, segToPending);
+    const [cursor, setCursor] = React.useState(null as null | Coord);
+
+    const points = cursor ? current.points.concat([cursor]) : current.points;
+    const seg = pendingToSeg({ ...current, points });
+
+    return (
+        <div>
+            <svg
+                width={300}
+                height={300}
+                onMouseMove={(evt) => {
+                    const box = evt.currentTarget.getBoundingClientRect();
+                    setCursor({
+                        x: snap(evt.clientX - box.left, grid),
+                        y: snap(evt.clientY - box.top, grid),
+                    });
+                }}
+                onMouseLeave={() => setCursor(null)}
+                onClick={(evt) => {
+                    const box = evt.currentTarget.getBoundingClientRect();
+                    const coord = {
+                        x: snap(evt.clientX - box.left, grid),
+                        y: snap(evt.clientY - box.top, grid),
+                    };
+                    const max = current.type === 'Line' ? 2 : 3;
+                    const changed = {
+                        ...current,
+                        points: current.points
+                            .slice(0, max - 1)
+                            .concat([coord]),
+                    };
+                    // console.log(max, changed, current);
+                    setCurrent(changed);
+                    const seg = pendingToSeg(changed);
+                    if (seg) {
+                        onChange(seg);
+                    }
+                }}
+            >
+                {children(
+                    seg,
+                    <>
+                        {seg ? (
+                            <RenderSegmentBasic
+                                zoom={1}
+                                prev={seg.prev}
+                                segment={seg.segment}
+                                inner={{
+                                    stroke: 'red',
+                                    strokeWidth: 4,
+                                }}
+                            />
+                        ) : null}
+                        {points.map((p, i) => (
+                            <circle
+                                key={i}
+                                cx={p.x}
+                                cy={p.y}
+                                fill="red"
+                                r={5}
+                            />
+                        ))}
+                    </>,
+                )}
+            </svg>
+            <div>
+                <button
+                    // disabled={current.type === 'Line'}
+                    style={
+                        current.type === 'Line' ? { fontWeight: 'bold' } : {}
+                    }
+                    onClick={() => {
+                        setCurrent({
+                            type: 'Line',
+                            points: [],
+                            clockwise: false,
+                        });
+                    }}
+                >
+                    Line
+                </button>
+                <button
+                    // disabled={current.type === 'Arc'}
+                    style={current.type === 'Arc' ? { fontWeight: 'bold' } : {}}
+                    onClick={() => {
+                        setCurrent({
+                            type: 'Arc',
+                            points: [],
+                            clockwise: false,
+                        });
+                    }}
+                >
+                    Arc
+                </button>
+                {cursor ? ' (' + cursor.x + ',' + cursor.y + ')' : null}
+            </div>
+        </div>
+    );
+};
+type Pending = {
+    type: 'Line' | 'Arc';
+    clockwise: boolean;
+    points: Array<Coord>;
+};
+
+export const pendingToSeg = (s: Pending): SegmentWithPrev | null => {
+    if (s.type === 'Line') {
+        if (s.points.length > 1) {
+            return {
+                prev: s.points[0],
+                segment: { type: 'Line', to: s.points[1] },
+                shape: -1,
+            };
+        }
+    } else {
+        if (s.points.length > 2) {
+            return {
+                prev: s.points[0],
+                shape: -1,
+                segment: {
+                    type: 'Arc',
+                    center: s.points[1],
+                    to: push(
+                        s.points[1],
+                        angleTo(s.points[1], s.points[2]),
+                        dist(s.points[0], s.points[1]),
+                    ),
+                    clockwise: s.clockwise,
+                },
+            };
+        }
+    }
+    return null;
+};
+
+export const segToPending = (s: SegmentWithPrev | null): Pending =>
+    s
+        ? {
+              type: s.segment.type,
+              clockwise: s.segment.type === 'Arc' ? s.segment.clockwise : false,
+              points:
+                  s.segment.type === 'Line'
+                      ? [s.prev, s.segment.to]
+                      : [s.prev, s.segment.center, s.segment.to],
+          }
+        : { type: 'Line', clockwise: false, points: [] };
+const snap = (v: number, grid: number) => Math.round(v / grid) * grid;
---
+++ src/rendering/atBottom.vest.tsx
@@ -0,0 +1,239 @@
+import * as React from 'react';
+import { segmentToPrimitive } from '../editor/findSelection';
+import { RenderSegmentBasic } from '../editor/RenderSegment';
+import { Coord } from '../types';
+import { register } from '../vest/vest';
+import { atCircleBottomOrSomething } from './atCircleBottomOrSomething';
+import { atLineBottom } from './clipPath';
+import { SegmentWithPrev } from './clipPathNew';
+import { dist } from './getMirrorTransforms';
+import { Circle, lineLine, lineToSlope, SlopeIntercept } from './intersect';
+import { SegmentEditor, useInitialState } from './SegmentEditor';
+
+type Pair = [SegmentWithPrev, Coord];
+type Which = 'segment' | 'coord';
+
+const Editor = ({
+    initial,
+    onChange,
+}: {
+    initial: Pair | null;
+    onChange: (pair: Pair) => void;
+}) => {
+    const [current, setCurrent] = useInitialState<
+        [SegmentWithPrev | null, Coord | null] | null
+    >(initial);
+    const segment = current ? current[0] : null;
+    const coord = current ? current[1] : null;
+
+    const [edit, setEdit] = React.useState('segment' as Which);
+
+    // React.useEffect(() => {
+    //     setEdit('segment');
+    // }, [initial]);
+
+    const buttons = (
+        <div>
+            <button
+                disabled={edit === 'segment'}
+                onClick={() => setEdit('segment')}
+            >
+                Segment
+            </button>
+            <button
+                disabled={edit === 'coord' || segment === null}
+                onClick={() => setEdit('coord')}
+            >
+                Coord
+            </button>
+        </div>
+    );
+
+    if (edit === 'segment') {
+        return (
+            <div>
+                <SegmentEditor
+                    onChange={(p) => setCurrent([p, null])}
+                    initial={segment}
+                >
+                    {(segment, rendered) => (
+                        <>
+                            {rendered}
+                            {coord && segment ? (
+                                <circle
+                                    cx={coord.x}
+                                    cy={coord.y}
+                                    fill={
+                                        isAtBottom(segment, coord)
+                                            ? 'green'
+                                            : 'red'
+                                    }
+                                    r={5}
+                                />
+                            ) : null}
+                        </>
+                    )}
+                </SegmentEditor>
+                {buttons}
+            </div>
+        );
+    } else {
+        return (
+            <div>
+                <CoordPlacer
+                    segment={segment!}
+                    coord={coord}
+                    onSet={(coord) => {
+                        setCurrent([segment!, coord]);
+                        onChange([segment!, coord]);
+                    }}
+                />
+                {buttons}
+            </div>
+        );
+    }
+};
+
+const CoordPlacer = ({
+    segment,
+    coord,
+    onSet,
+}: {
+    segment: SegmentWithPrev;
+    coord: Coord | null;
+    onSet: (coord: Coord) => void;
+}) => {
+    const [cursor, setCursor] = React.useState(null as null | Coord);
+
+    const clean = (coord: Coord): Coord | null => {
+        const margin = 5;
+        if (dist(coord, segment.prev) < margin) {
+            return segment.prev;
+        }
+        if (dist(coord, segment.segment.to) < margin) {
+            return segment.segment.to;
+        }
+        if (segment.segment.type === 'Line') {
+            const si = lineToSlope(segment.prev, segment.segment.to, true);
+            const other: SlopeIntercept =
+                si.m === 0
+                    ? { type: 'line', m: Infinity, b: coord.x }
+                    : { type: 'line', m: 0, b: coord.y };
+            const int = lineLine(si, other);
+            return int;
+        }
+        return null;
+    };
+
+    return (
+        <svg
+            width={300}
+            height={300}
+            onMouseMove={(evt) => {
+                const box = evt.currentTarget.getBoundingClientRect();
+                setCursor(
+                    clean({
+                        x: evt.clientX - box.left,
+                        y: evt.clientY - box.top,
+                    }),
+                );
+            }}
+            onMouseLeave={() => setCursor(null)}
+            onClick={(evt) => {
+                const box = evt.currentTarget.getBoundingClientRect();
+                const coord = clean({
+                    x: evt.clientX - box.left,
+                    y: evt.clientY - box.top,
+                });
+                if (coord) {
+                    onSet(coord);
+                }
+            }}
+        >
+            {segment ? (
+                <RenderSegmentBasic
+                    zoom={1}
+                    prev={segment.prev}
+                    segment={segment.segment}
+                    inner={{
+                        stroke: 'blue',
+                        strokeWidth: 4,
+                    }}
+                />
+            ) : null}
+            {cursor ? (
+                <circle
+                    cx={cursor.x}
+                    cy={cursor.y}
+                    fill={isAtBottom(segment, cursor) ? 'green' : 'red'}
+                    r={5}
+                />
+            ) : null}
+            {coord ? (
+                <circle
+                    cx={coord.x}
+                    cy={coord.y}
+                    fill={isAtBottom(segment, coord) ? 'green' : 'red'}
+                    r={5}
+                />
+            ) : null}
+        </svg>
+    );
+};
+
+const Fixture = ({
+    input,
+    output,
+    previous,
+}: {
+    input: Pair;
+    output: boolean;
+    previous: { output: boolean | null; isPassing: boolean };
+}) => {
+    return (
+        <div>
+            <svg width={300} height={300}>
+                <RenderSegmentBasic
+                    zoom={1}
+                    prev={input[0].prev}
+                    segment={input[0].segment}
+                    inner={{
+                        stroke: 'blue',
+                        strokeWidth: 4,
+                    }}
+                />
+                <circle
+                    cx={input[1].x}
+                    cy={input[1].y}
+                    r={5}
+                    fill={output ? 'green' : 'red'}
+                />
+            </svg>
+        </div>
+    );
+};
+
+const isAtBottom = (segment: SegmentWithPrev, coord: Coord) => {
+    return segment.segment.type === 'Line'
+        ? atLineBottom(
+              coord,
+              segmentToPrimitive(
+                  segment.prev,
+                  segment.segment,
+              ) as SlopeIntercept,
+          )
+        : atCircleBottomOrSomething(
+              coord,
+              segmentToPrimitive(segment.prev, segment.segment) as Circle,
+          );
+};
+
+register<Pair, boolean>({
+    id: 'atBottom',
+    dir: __dirname,
+    transform: ([segment, coord]) => isAtBottom(segment, coord),
+    render: {
+        editor: Editor,
+        fixture: Fixture,
+    },
+});
