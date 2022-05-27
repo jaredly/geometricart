300f770d87ded5e0c63f363bb526fe8efac1e3c1
Jared Forsyth <jared@jaredforsyth.com>
2022-05-27 12:44:36 -0500

    round

--- src/rendering/CanvasRender.tsx
+++ src/rendering/CanvasRender.tsx
@@ -195,6 +195,8 @@ export const canvasRender = async (
                     }
                 }
 
+                ctx.lineJoin = 'round';
+                ctx.lineCap = 'round';
                 ctx.beginPath();
                 tracePath(ctx, myPath, zoom);
 
@@ -270,6 +272,8 @@ export const canvasRender = async (
                 ctx.beginPath();
                 tracePath(ctx, myPath, zoom);
                 ctx.strokeStyle = color;
+                ctx.lineJoin = 'round';
+                ctx.lineCap = 'round';
                 ctx.stroke();
             }
             ctx.globalAlpha = 1;
