4b1182edc4b443fc7ee4892323f9d677a66fe83d
Jared Forsyth <jared@jaredforsyth.com>
2022-10-22 05:16:12 +0000

    [3d-render] yassss love it

--- src/gcode/GCode3D.tsx
+++ src/gcode/GCode3D.tsx
@@ -2,13 +2,42 @@ import * as React from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
 import {
     CanvasTexture,
+    DoubleSide,
     Mesh,
     MeshBasicMaterial,
     RepeatWrapping,
+    ShaderMaterial,
     Texture,
 } from 'three';
 import { GCodeData, renderCutDepths } from './Visualize';
 import { OrbitControls } from '@react-three/drei';
+
+// Based on https://stemkoski.github.io/Three.js/Shader-Heightmap-Textures.html
+const vertext = `
+uniform sampler2D bumpTexture;
+uniform float bumpScale;
+
+varying vec2 vUV;
+
+void main() { 
+	vUV = uv;
+	vec4 bumpData = texture2D( bumpTexture, uv );
+	
+	// move the position along the normal
+    vec3 newPosition = position + normal * bumpScale * bumpData.r;
+	
+	gl_Position = projectionMatrix * modelViewMatrix * vec4( newPosition, 1.0 );
+}
+`;
+
+const fragment = `
+uniform sampler2D bumpTexture;
+varying vec2 vUV;
+
+void main() {
+	gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0) + texture2D(bumpTexture, vUV);
+}  
+`;
 
 export const GCode3D = ({
     data,
@@ -19,7 +48,7 @@ export const GCode3D = ({
 }) => {
     const tx = React.useMemo(() => {
         const canvas = document.createElement('canvas');
-        const scale = 5;
+        const scale = 10;
         canvas.width = data.dims.width * scale;
         canvas.height = data.dims.height * scale;
         const ctx = canvas.getContext('2d')!;
@@ -36,26 +65,52 @@ export const GCode3D = ({
         // tx.repeat.set(1 / width, 1 / height);
         return tx;
     }, []);
+
     return (
         <div style={{ width: 500, height: 500, border: '1px solid magenta' }}>
             <Canvas>
                 <ambientLight />
                 <pointLight position={[10, 10, 10]} />
-                <VBox tx={tx} />
+                <VBox tx={tx} data={data} />
                 <OrbitControls />
             </Canvas>
         </div>
     );
 };
 
-function VBox({ tx }: { tx: Texture }) {
+function VBox({ tx, data }: { tx: Texture; data: GCodeData }) {
+    const scale = 10;
     const mesh = React.useRef<Mesh>(null);
-    useFrame((state, delta) => (mesh.current!.rotation.x += 0.01));
+    // useFrame((state, delta) => (mesh.current!.rotation.x += 0.01));
+
+    const mat = React.useMemo(() => {
+        const customUniforms = {
+            bumpTexture: { type: 't', value: tx },
+            bumpScale: { type: 'f', value: 1.0 },
+        };
+        const mat = new ShaderMaterial({
+            uniforms: customUniforms,
+            vertexShader: vertext,
+            fragmentShader: fragment,
+            side: DoubleSide,
+        });
+        return mat;
+    }, []);
 
     return (
-        <mesh ref={mesh}>
-            <meshBasicMaterial map={tx} toneMapped={false} />
-            <boxGeometry args={[2, 2, 2]} />
+        <mesh ref={mesh} material={mat}>
+            {/* <meshBasicMaterial material={tx} toneMapped={false} /> */}
+            {/* <boxGeometry args={[2, 2, 2]} /> */}
+            <planeGeometry
+                args={[
+                    5,
+                    5,
+                    // data.dims.width * scale,
+                    // data.dims.height * scale,
+                    data.dims.width * scale,
+                    data.dims.height * scale,
+                ]}
+            />
         </mesh>
     );
 }
