2994c59525d4965c51cff9b1c0ebccacae23a1d3
Jared Forsyth <jared@jaredforsyth.com>
2022-11-22 00:03:29 -0600

    [newui] trying out primeicons ... hmm

--- src/NewSidebar.tsx
+++ src/NewSidebar.tsx
@@ -1,7 +1,8 @@
-import { Divider, Paper, Radio, Text } from '@mantine/core';
+import { Card } from 'primereact/card';
 import * as React from 'react';
 import { Action } from './state/Action';
 import { State } from './types';
+import { RadioButton } from 'primereact/radiobutton';
 
 export const NewSidebar = ({
     state,
@@ -12,42 +13,40 @@ export const NewSidebar = ({
 }): JSX.Element => {
     return (
         <div style={{ minWidth: 200, padding: 16, background: '#888' }}>
-            <Paper>
-                <Text p="xs" c="dimmed" fz="xs">
-                    Mirrors
-                </Text>
-                <Divider variant="dashed" />
+            <Card>
+                <span>Mirrors</span>
                 {Object.entries(state.mirrors).map(([k, mirror]) => (
-                    <Radio
-                        p="xs"
-                        checked={state.activeMirror === k}
-                        style={{ cursor: 'pointer' }}
-                        onClick={() => {
-                            if (state.activeMirror !== k) {
-                                dispatch({ type: 'mirror:active', id: k });
-                            } else {
-                                dispatch({ type: 'mirror:active', id: null });
-                            }
-                        }}
-                        label={
-                            <Text key={k}>
-                                {mirror.rotational.length}x at{' '}
-                                {mirror.origin.x.toFixed(2)},
-                                {mirror.origin.y.toFixed(2)}
-                            </Text>
-                        }
-                    />
+                    <div key={k} className="field-radiobutton">
+                        <RadioButton
+                            checked={state.activeMirror === k}
+                            inputId={k}
+                            onChange={(evt) => {
+                                if (state.activeMirror !== evt.value) {
+                                    dispatch({
+                                        type: 'mirror:active',
+                                        id: evt.value,
+                                    });
+                                } else {
+                                    dispatch({
+                                        type: 'mirror:active',
+                                        id: null,
+                                    });
+                                }
+                            }}
+                            name="mirror"
+                            value={k}
+                        />
+                        <label htmlFor={k}>
+                            {mirror.rotational.length}x at{' '}
+                            {mirror.origin.x.toFixed(2)},
+                            {mirror.origin.y.toFixed(2)}
+                        </label>
+                    </div>
                 ))}
-                <Divider />
-                <Text p="xs" c="dimmed">
-                    Guides
-                </Text>
-                <Divider />
-                <Text p="xs" c="dimmed">
-                    Shapes
-                </Text>
-            </Paper>
-            Sidebarz
+                {/* <Divider /> */}
+                <span>Guides</span>
+                <span>Shapes</span>
+            </Card>
         </div>
     );
 };
--- src/run.tsx
+++ src/run.tsx
@@ -1,21 +1,24 @@
 // Basic ... ideas ...
 
-import { MantineProvider } from '@mantine/core';
+// import { MantineProvider } from '@mantine/core';
 import * as React from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
 import { loadInitialState } from './state/persistence';
+import 'primereact/resources/themes/bootstrap4-dark-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
 
 const root = createRoot(document.getElementById('root')!);
 
 loadInitialState().then((state) =>
     root.render(
-        <MantineProvider
-            theme={{ colorScheme: 'dark' }}
-            withGlobalStyles
-            withNormalizeCSS
-        >
-            <App initialState={state} />
-        </MantineProvider>,
+        // <MantineProvider
+        //     theme={{ colorScheme: 'dark' }}
+        //     withGlobalStyles
+        //     withNormalizeCSS
+        // >
+        <App initialState={state} />,
+        // </MantineProvider>,
     ),
 );
