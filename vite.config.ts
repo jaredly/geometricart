07662048d38732fb9bdee4632e1b10a906b88512
Jared Forsyth <jared@jaredforsyth.com>
2025-08-25 06:42:52 -0500

    hm

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import {defineConfig} from 'vite';
+
+export default defineConfig({
+    // ...
+    build: {
+        target: 'esnext', // you can also use 'es2020' here
+    },
+    optimizeDeps: {
+        esbuildOptions: {
+            target: 'esnext', // you can also use 'es2020' here
+        },
+    },
+});
