e2eeac4c505cb1be315bb68018ded3aa7944ff0e
Jared Forsyth <jared@jaredforsyth.com>
2025-08-25 06:44:23 -0500

    more tyepss

--- src/animation/Lerps.tsx
+++ src/animation/Lerps.tsx
@@ -1,61 +1,35 @@
-import React, { useMemo } from 'react';
+import React, {useMemo} from 'react';
+// @ts-expect-error
 import prettier from 'prettier';
+// @ts-expect-error
 import babel from 'prettier/parser-babel';
-import { BlurInt } from '../editor/Forms';
-import { Action } from '../state/Action';
-import { FloatLerp, ScriptLerp, LerpPoint, State, PosScript } from '../types';
-import { PointsEditor, pointsPathD } from './PointsEditor';
-import { AddVbl } from './AnimationUI';
-import { functionWithBuiltins } from './getAnimatedPaths';
+import {BlurInt} from '../editor/Forms';
+import {Action} from '../state/Action';
+import {FloatLerp as FLT, ScriptLerp as SLT, LerpPoint, State, PosScript} from '../types';
+import {PointsEditor, pointsPathD} from './PointsEditor';
+import {AddVbl} from './AnimationUI';
+import {functionWithBuiltins} from './getAnimatedPaths';
 
-export function Lerps({
-    dispatch,
-    state,
-}: {
-    dispatch: (action: Action) => unknown;
-    state: State;
-}) {
+export function Lerps({dispatch, state}: {dispatch: (action: Action) => unknown; state: State}) {
     return (
-        <div style={{ flex: 1, overflow: 'auto' }}>
+        <div style={{flex: 1, overflow: 'auto'}}>
             <AddVbl
                 onAdd={(key, vbl) => {
-                    dispatch({ type: 'timeline:update', key, vbl });
+                    dispatch({type: 'timeline:update', key, vbl});
                 }}
             />
             {Object.keys(state.animations.lerps).map((key) => {
                 const vbl = state.animations.lerps[key];
                 if (vbl.type !== 'float') {
-                    return (
-                        <ScriptLerp
-                            key={key}
-                            id={key}
-                            vbl={vbl}
-                            dispatch={dispatch}
-                        />
-                    );
+                    return <ScriptLerp key={key} id={key} vbl={vbl} dispatch={dispatch} />;
                 }
-                return (
-                    <FloatLerp
-                        key={key}
-                        id={key}
-                        vbl={vbl}
-                        dispatch={dispatch}
-                    />
-                );
+                return <FloatLerp key={key} id={key} vbl={vbl} dispatch={dispatch} />;
             })}
         </div>
     );
 }
 
-function ScriptLerp({
-    id: key,
-    vbl,
-    dispatch,
-}: {
-    id: string;
-    vbl: ScriptLerp | PosScript;
-    dispatch: (action: Action) => unknown;
-}): JSX.Element {
+function ScriptLerp({id: key, vbl, dispatch}: {id: string; vbl: SLT | PosScript; dispatch: (action: Action) => unknown}): JSX.Element {
     const [code, setCode] = React.useState(vbl.code);
 
     const fn = useMemo(() => {
@@ -74,10 +48,10 @@ function ScriptLerp({
                 border: '1px solid #aaa',
             }}
         >
-            <div style={{ marginBottom: 4 }}>{key}</div>
+            <div style={{marginBottom: 4}}>{key}</div>
             <textarea
                 value={code}
-                style={{ width: 400 }}
+                style={{width: 400}}
                 onChange={(evt) => {
                     setCode(evt.target.value);
                 }}
@@ -95,14 +69,14 @@ function ScriptLerp({
                     dispatch({
                         type: 'timeline:update',
                         key,
-                        vbl: { ...vbl, code: formatted },
+                        vbl: {...vbl, code: formatted},
                     });
                 }}
             />
             <FnViewer fn={fn} kind={vbl.type} />
             <button
                 onClick={() => {
-                    dispatch({ type: 'timeline:update', key, vbl: null });
+                    dispatch({type: 'timeline:update', key, vbl: null});
                 }}
             >
                 Delete
@@ -111,21 +85,13 @@ function ScriptLerp({
     );
 }
 
-function FloatLerp({
-    id: key,
-    vbl,
-    dispatch,
-}: {
-    id: string;
-    vbl: FloatLerp;
-    dispatch: (action: Action) => unknown;
-}): JSX.Element {
-    const [current, setCurrentInner] = React.useState(null as null | FloatLerp);
+function FloatLerp({id: key, vbl, dispatch}: {id: string; vbl: FLT; dispatch: (action: Action) => unknown}): JSX.Element {
+    const [current, setCurrentInner] = React.useState(null as null | FLT);
     const last = React.useRef(vbl.points);
     React.useEffect(() => {
         if (last.current !== vbl.points) {
             last.current = vbl.points;
-            setCurrentInner((c) => (c ? { ...c, points: vbl.points } : c));
+            setCurrentInner((c) => (c ? {...c, points: vbl.points} : c));
         }
     }, [vbl.points]);
 
@@ -139,13 +105,10 @@ function FloatLerp({
                 }}
             >
                 {key}
-                <PointsViewer
-                    onClick={() => setCurrentInner(vbl)}
-                    points={vbl.points}
-                />
+                <PointsViewer onClick={() => setCurrentInner(vbl)} points={vbl.points} />
                 <button
                     onClick={() => {
-                        dispatch({ type: 'timeline:update', key, vbl: null });
+                        dispatch({type: 'timeline:update', key, vbl: null});
                     }}
                 >
                     Delete
@@ -217,23 +180,15 @@ function FloatLerp({
                 current={current.points}
                 setCurrentInner={(points) =>
                     typeof points === 'function'
-                        ? setCurrentInner((v) =>
-                              v ? { ...v, points: points(v.points) } : v,
-                          )
-                        : setCurrentInner((v) => (v ? { ...v, points } : v))
+                        ? setCurrentInner((v) => (v ? {...v, points: points(v.points)} : v))
+                        : setCurrentInner((v) => (v ? {...v, points} : v))
                 }
             />
         </div>
     );
 }
 
-export const FnViewer = ({
-    fn,
-    kind,
-}: {
-    fn: (n: number) => number;
-    kind: 'float-fn' | 'pos-fn';
-}) => {
+export const FnViewer = ({fn, kind}: {fn: (n: number) => number; kind: 'float-fn' | 'pos-fn'}) => {
     const width = 150;
     const height = kind === 'float-fn' ? 50 : 150;
     const margin = 5;
@@ -247,7 +202,7 @@ export const FnViewer = ({
                 } else {
                     const x = i / width;
                     const y = fn(x);
-                    points.push({ x, y });
+                    points.push({x, y});
                 }
             }
             return points;
@@ -260,32 +215,17 @@ export const FnViewer = ({
         <svg
             width={width + margin * 2}
             height={height + margin * 2}
-            viewBox={`${-margin} ${-margin} ${width + margin * 2} ${
-                height + margin * 2
-            }`}
+            viewBox={`${-margin} ${-margin} ${width + margin * 2} ${height + margin * 2}`}
             style={{
                 border: '1px solid #333',
             }}
         >
-            <polyline
-                points={points
-                    .map((m) => `${m.x * width},${(1 - m.y) * height}`)
-                    .join(' ')}
-                stroke="red"
-                strokeWidth={2}
-                fill="none"
-            />
+            <polyline points={points.map((m) => `${m.x * width},${(1 - m.y) * height}`).join(' ')} stroke="red" strokeWidth={2} fill="none" />
         </svg>
     );
 };
 
-export const PointsViewer = ({
-    points,
-    onClick,
-}: {
-    points: Array<LerpPoint>;
-    onClick: () => void;
-}) => {
+export const PointsViewer = ({points, onClick}: {points: Array<LerpPoint>; onClick: () => void}) => {
     const width = 50;
     const height = 50;
 
