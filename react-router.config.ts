186337771d0675c84a86bf54a2820c19bcebf255
Jared Forsyth <jared@jaredforsyth.com>
2025-11-15 15:37:48 -0600

    yay making progress on static gen

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,9 +1,13 @@
 import type {Config} from '@react-router/dev/config';
+import {getAllPatterns, getPattern} from './src/routes/db.server';
 // import {Database} from 'bun:sqlite';
 
 // const db = new Database(join(import.meta.dirname, 'data.db'));
 // const query = db.query('select id, hash from Tiling');
 // const alls = query.all() as {id: string; hash: string}[];
+const alls = getAllPatterns();
+
+// const images = alls.map((t) => getPattern(t.hash)).flatMap((p) => p?.images);
 
 export default {
     // Config options...
@@ -14,7 +18,15 @@ export default {
     //     return [...getStaticPaths(), ...alls.map((tiling) => `gallery/pattern/${tiling.hash}`)];
     // },
     async prerender() {
-        return ['/animator/', '/gallery/'];
+        return [
+            '/',
+            '/animator/',
+            '/gallery/',
+            ...alls.map((tiling) => `/gallery/pattern/${tiling.hash}`),
+            ...alls.map((tiling) => `/gallery/pattern/${tiling.hash}/600.png`),
+            // ...alls.map((tiling) => `/gallery/pattern/${tiling.hash}/400.png`),
+            // ...(images.map((im) => im?.url).filter(Boolean) as string[]),
+        ];
     },
     appDirectory: 'src/routes',
 } satisfies Config;
